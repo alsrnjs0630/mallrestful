4db086f9d5178e7cf173795509868865461f39e9
qkdalsrnjs7 <qkdalsrnjs7@naver.com>
2024-08-22 15:55:01 +0900

    Changes

---
+++ src/main/java/com/lab/mallrestful/MallrestfulApplication.java
@@ -0,0 +1,13 @@
+package com.lab.mallrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallrestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallrestfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/mallrestful/MallrestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.mallrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallrestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
